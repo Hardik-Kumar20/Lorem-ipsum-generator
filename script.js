b48db65049898efa5cd22b39d254e7fa8a2ab243
Hardik Kumar <kumarhardik821@gmail.com>
2025-01-01 12:15:03 -0800

    new js and css updates

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ alert("hello")
 let container = document.querySelector(".container");
 let paragraph = document.querySelector(".para");
 let paragraphSlider = document.getElementById("Paragraphs")
-let paragraphValue = document.querySelector(".paragraphValue");
+let paragraphValue = document.getElementById("paragraphValue");
 let words = document.querySelector(".words");
-let wordSlider = document.getElementById(".words")
+let wordSlider = document.getElementById("words")
 let wordValue = document.querySelector(".wordsValue");
 let tagSelect = document.getElementById("tags");
+let output = document.querySelector(".output")
 
 function createOptionUi(){
     tagOptions.forEach(tags => {
@@ -53,13 +54,45 @@ function createOptionUi(){
 
     for (let i = 0; i < paragraphs; i++) {
         const words = generatepara(wordsperpara);
-        
+        if (include === "Yes") {
+            loremIpsumArray[i] = `<${tag}>${words}</${tag}>`;
+        } else {
+            loremIpsumArray[i] = words;
+        }   
     }
+    // join paragraphs into single pharagraph
+    return loremIpsumArray.join("\n");
     }
     
+    // Function to generate a specified number of words
+function generatepara(numWords) {
+    
+  
+    const loremIpsumText =`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, est! Quas, repudiandae a? Consequatur corporis, voluptas eius facere temporibus magni quod, cumque quia consequuntur minus voluptatem a porro exercitationem neque.`;
 
+
+    // Split the Lorem Ipsum text into words
+    const words =
+        loremIpsumText.split(" ");
+
+    // Ensure the number of words requested is 
+    // within the bounds of the available words
+    if (numWords <= words.length) {
+        return words
+            .slice(0, numWords)
+            .join(" ");
+    } else {
+        return words.join(" ");
+    }
 }
 
 
+// Now display the lorem Ipsum text
+    function displayLoremText(loremText) {
+        output.innerHTML = loremText;
+    }
+}
 
+
+createOptionUi();
 
